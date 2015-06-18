bd5b11a91a639b4a236f58e2c6f5c3ea6f5d1152
patat <nemnogosloven@gmail.com>
2015-06-18 20:57:44 +0300

    ...

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var wikiGame = angular.module( 'wikiGame', []);
 wikiGame.controller( 'wikiCtrl', function($scope, $http) {
   $scope.wikiContent = [];
 
-  var url = 'https://ru.wikipedia.org/w/api.php?action=query&titles=Финал_Кубка_Стэнли_2015&prop=revisions&rvprop=content&format=json';
+  var url = 'https://en.wikipedia.org/w/api.php?action=query&titles=Blue%27s_Clues&prop=revisions&rvprop=content&format=json';
 
   $http.jsonp( url )
     .success(function(data) {
