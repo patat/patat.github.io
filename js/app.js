af8ba0c6437112d476e8c67c0b71adb7cf058fcb
patat <nemnogosloven@gmail.com>
2015-06-18 20:31:56 +0300

    ...

--- js/app.js
+++ js/app.js
@@ -5,14 +5,14 @@ wikiGame.controller( 'wikiCtrl', function($scope, $http) {
 
   var url = 'https://ru.wikipedia.org/wiki/%D0%A4%D0%B8%D0%BD%D0%B0%D0%BB_%D0%9A%D1%83%D0%B1%D0%BA%D0%B0_%D0%A1%D1%82%D1%8D%D0%BD%D0%BB%D0%B8_2015';
 
-  // $http.get( url )
-  //   .success( function( response ) {
-  //     $scope.wikiContent = response;
-  //     console.log($scope.wikiContent);
-  //   } );
+  $http.get( url )
+    .success( function( response ) {
+      
+      console.log( response );
+    } );
 
-  $http.jsonp( url )
-    .success(function(data) {
-      console.log(data);
-    });
+  // $http.jsonp( url )
+  //   .success(function(data) {
+  //     console.log(data);
+  //   });
 });
