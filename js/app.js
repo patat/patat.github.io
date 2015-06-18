dd0bf3c992ca0554d0b2947af57363ba03615b7f
patat <nemnogosloven@gmail.com>
2015-06-18 21:03:44 +0300

    ...

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var wikiGame = angular.module( 'wikiGame', []);
 wikiGame.controller( 'wikiCtrl', function($scope, $http) {
   $scope.wikiContent = [];
 
-  var url = 'https://en.wikipedia.org/w/api.php?action=query&titles=Blue%27s_Clues&prop=revisions&rvprop=content&format=json';
+  var url = 'https://ru.wikipedia.org/w/api.php?callback=JSON_CALLBACK&action=query&titles=Сковородка_(гитара)&prop=revisions&rvprop=content&format=json';
 
   $http.jsonp( url )
     .success(function(data) {
